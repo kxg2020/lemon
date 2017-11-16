7544f532eca6425a6bc46602ad2faf6bf2dd8949
lostinfo <1225039937@qq.com>
2017-11-16 11:56:09 +0800

    PUBG

--- resources/assets/backend/js/components/Main.vue
+++ resources/assets/backend/js/components/Main.vue
@@ -8,6 +8,13 @@
                 <span>浏览量：</span><span class="coral">{{page_view}}</span>
             </div>
         </el-card>
+        <el-card class="box-card" v-loading="pubgLoading">
+            <el-carousel indicator-position="outside" :height="pubgHeight" id="pubg">
+                <el-carousel-item v-for="(item, index) in pubgImages" :key="index">
+                    <img class="pubg-img" :src="imgPrefix + item.file_name" alt="" width="100%">
+                </el-carousel-item>
+            </el-carousel>
+        </el-card>
     </el-row>
 </template>
 <style>
@@ -21,18 +28,37 @@
     .item {
         padding: 18px 0;
     }
+    .pubg-img {
+        width: 100%;
+        height: auto;
+    }
+    .box-card {
+        margin-bottom: 20px;
+    }
 </style>
 <script type="text/ecmascript-6">
     export default{
         data(){
             return {
                 listLoading: true,
+                pubgLoading: true,
                 post_count: 0,
                 page_view: 0,
+                pubgImages: [],
+                imgPrefix: 'http://img.it9g.com/',
+                pubgHeight: '400px'
+            }
+        },
+        created: function () {
+            let _this = this
+            window.onresize = function () {
+                _this.widthChange()
             }
         },
         mounted() {
             this.getDatas()
+            this.getPubgImages()
+            this.widthChange()
         },
         methods: {
             getDatas() {
@@ -50,6 +76,27 @@
                         })
                     }
                 })
+            },
+            getPubgImages() {
+                let _this = this
+                _this.axios.get('/files', {params: {dir: 'pubg', 'page_size': 20, 'page': 1}})
+                  .then(function (response) {
+                      let res = response.data
+                      if(res.status == 'success') {
+                          _this.pubgImages = res.data.data
+                          _this.pubgLoading = false
+                      }else {
+                          _this.$message({
+                              message: "获取数据失败",
+                              type: 'error'
+                          })
+                      }
+                  })
+            },
+            widthChange() {
+                let pubgDom = document.getElementById("pubg")
+                let pubgWidth = pubgDom.clientWidth
+                this.pubgHeight = (pubgWidth * 9 / 16) + "px"
             }
         }
     }
