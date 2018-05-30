40375ad02d840818cbb6dfe37b216e3c8b446d22
lostinfo <1225039937@qq.com>
2018-05-30 09:43:55 +0800

    logging

--- app/Http/Controllers/Api/CommentsController.php
+++ app/Http/Controllers/Api/CommentsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Model\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class CommentsController extends Controller
 {
